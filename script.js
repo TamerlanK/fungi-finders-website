10fe83756ea7b2822d065ab7270e209828f732f6
Tamerlan Kengerli <tamerlankengerli2002@gmail.com>
2025-02-25 03:49:55 +0400

    Add responsive navigation toggle and improve accessibility features

---
+++ script.js
@@ -0,0 +1,9 @@
+const navToggle = document.querySelector('[aria-controls="primary-nav"]')
+const primaryNav = document.getElementById("primary-nav")
+
+navToggle.addEventListener("click", () => {
+  const expanded = navToggle.getAttribute("aria-expanded") === "true" || false
+
+  navToggle.setAttribute("aria-expanded", !expanded)
+  primaryNav.hidden = expanded
+})
